cacad7736870ec56e1735cb58abbff6733496ead
Luis Alberto <albertogtelesforo@gmail.com>
2023-06-08 13:08:32 -0600

    Update main.py
    Changes on comments for implementation

--- src/wisetree/main.py
+++ src/wisetree/main.py
@@ -11,41 +11,47 @@ The main function.
 data : pairs (x,y)
 features : features of x
 
-returs: a tree
+returns: a tree
 """
-#TODO what types are data and features?
-#TODO what is a tree?
+##TODO implement data and features with numpy
 def wise_tree(data,features):
-    #TODO tree is a data structure
-    root = tree();
-    #TODO .trivial() determines whether all data lies in the same class
+    #TODO the node data structure as a class
+    root = node();
+    ##TODO .trivial() determines whether all data lies in the same label
+    ##implement trivial with numpy
     if (data.trivial()):
-        #TODO leaf is a marker;
+        #TODO leaf is a subclass of node
         #it avoids splitting;
-        #also, it adds a label for the class
-        #TODO .majority_class_label() choose the label with the majority data;
-        #in this case all data must have that label
+        #also, it adds a label for the data
+        ##TODO .majority_class_label() choose the label with the majority data;
+        ##in this case all data must have that label
+        ##implement with numpy
         root.leaf(data.majority_class_label());
         return root;
-    #TODO empty determines if we have no features
-    #TODO .Features() extracts al values on the given features
-    #TODO .size() we need the ability to count our observations
+    ##TODO .empty determines if we have no features
+    ##implement with numpy
+    ##TODO .Features() extracts al values on the given features
+    ##implement with numpy
+    ##TODO .size() we need the ability to count our observations
+    ##implement with numpy
     if (features.empty() || data.Features(features).size()==1):
         root.leaf(data.class_label(data.Features(features)));
         return root;
     #TODO .optimal_split_feat() determines the better feature to start splitting
-    #TODO what is a feature?
     optimal_feature = features.optimal_split_feat();
-    #TODO .feature() extracts the values of the given feature
-    for observation in data.feature(optimal_feature):
-        #TODO .where() selects the subset with the given value for the given feature
+    ##TODO data.optimal_feature as in Pandas
+    ##implement with numpy
+    for observation in data.optimal_feature:
+        ##TODO .where() is a boolean mask
+        ##implement with numpy
         data_observation = data.where(optimal_feature,observation);
         if (data_observation.size()==0):
             #TODO .add_leafs() adds a leaf to a tree
             branch = root.add_leaf(data.majority_class_label()); 
             return root;
         else:
-            #TODO .pop() pops a feature
+            ##TODO .pop() pops a feature
+            ##implement with numpy
             branch = wise_tree(data_observation,features.pop(optimal_feature));
             #TODO .add_branch() pastes two trees
             root.add_branch(branch)
